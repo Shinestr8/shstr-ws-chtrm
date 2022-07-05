dcb6091a07b47c242976a699f0150d520995bf29
Corentin <shinedownizstr8@gmail.com>
2022-07-05 15:21:05 +0200

    improve chatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,84 +1,30 @@
 import './App.css';
-import { w3cwebsocket as W3CWebSocket } from "websocket";
-import {useEffect, useState} from "react"
-const client = new W3CWebSocket('ws://192.168.1.20:8000');
-
-
+import { Chat } from './Chat';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { Login } from './Login';
+import { useState } from 'react';
 
 function App() {
-  // componentWillMount() {
-  //   client.onopen = () => {
-  //     console.log('WebSocket Client Connected');
-  //   };
-  //   client.onmessage = (message) => {
-  //     console.log(message);
-  //   };
-  // }
 
-  const [message, setMessage] = useState(undefined)
-  const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState("");
+  const [color, setColor] = useState("");
 
-
-  useEffect(function(){
-    client.onopen = ()=>{
-      console.log("Websocket client connected")
-    };
-    client.onmessage = (message)=>{
-      setMessages(messages => [...messages, message.data])
-    };
-  }, [])
-
-  
-
-  function handleTextAreaChange(e){
-    setMessage(e.target.value)
+  function handleUsername(u){
+    setUser(u);
   }
 
-//   logInUser = () => {
-//     const username = this.username.value;
-//     if (username.trim()) {
-//         const data = {
-//             username
-//         };
-//         this.setState({
-//             ...data
-//         }, () => {
-//             client.send(JSON.stringify({
-//                 ...data,
-//                 type: "userevent"
-//             }));
-//         });
-//     }
-// }
-  function sendMessage(e){
-    e.preventDefault();
-    client.send(JSON.stringify({message, type: "userevent"}))
-    setMessage("")
+  function handleColor(c){
+    setColor(c);
   }
 
-  return (
-    <div className="chatting">
-      <div className="msg-history">
-        {/* {JSON.stringify(messages)} */}
-        {messages.map(function(msg, index){
-          return <div className='msg' key={index}>{msg}</div>
-        })}
-      </div>
-      <div className="text-box">
-        <form>
-        <input
-          type="text"
-          value={message}
-          onChange={handleTextAreaChange}
-          placeholder="type your message here..."/
-        >
-        <button onClick={sendMessage}>
-          Send
-        </button>
-        </form>
-        
-      </div>
-    </div>
+
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login setUser={handleUsername} setColor={handleColor}/>}/>
+        <Route path="/chat" element={<Chat user={user} color={color}/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
